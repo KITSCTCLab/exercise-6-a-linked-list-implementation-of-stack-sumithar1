7196dcab643fb8318e186d93b37f8be3730848f9
sumithar1 <100009512+sumithar1@users.noreply.github.com>
2022-09-09 10:21:54 +0530

    Initial commit

--- Main.py
+++ Main.py
@@ -9,16 +9,23 @@ class Stack:
     self.head = None
 
   def push(self, data) -> None:
-    # Write your code here
+    new = Node(data)
+    new.next = self.head
+    self.head = new
+    
 
   def pop(self) -> None:
-    # Write your code here
+    if not self.head is None:
+        self.head = self.head.next
 
   def status(self):
     """
     It prints all the elements of stack.
     """
-    # Write your code here  
+    current = self.head
+    while current is not None:
+        print(current.data, end = "=>")
+    print("None")
 
 
 # Do not change the following code
